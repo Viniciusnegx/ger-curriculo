69c4d775ea58a2519e6819f691112dd38237939f
Viniciusnegx <vininegx@gmail.com>
2024-06-25 14:34:07 -0300

    Iniciando projeto

---
+++ gerarCurriculo.php
@@ -0,0 +1,43 @@
+<?php
+
+$modelo = $_POST['modelo'];
+
+
+if($_FILES['foto']['size'] > 0){
+    
+    $conteudo = file_get_contents($_FILES['foto']['tmp_name']);
+    
+    
+    $tipo = pathinfo($_FILES['foto']['tmp_name'], PATHINFO_EXTENSION);
+    
+    
+    $foto = 'data:image/' . $tipo . ';base64,' . base64_encode($conteudo);
+} else {
+    $foto = "img/avatar-1.png";
+}
+
+$dados = array(
+    'foto' => $foto,
+    'nome' => $_POST['nome'],
+    'cargo' => $_POST['cargo'],
+    'idade' => $_POST['idade'],
+    'endereco' => $_POST['endereco'],
+    'telefone' => $_POST['telefone'],
+    'email' => $_POST['email'],
+    'resumo' => $_POST['resumo'],
+    'formacoes' => isset($_POST['formacao-curso']) ?
+                   array(
+                        'cursos' => $_POST['formacao-curso'],
+                        'instituicoes' => $_POST['formacao-instituicao'],
+                        'conclusoes' => $_POST['formacao-conclusao']
+                    ) : null,
+    'experiencias' => isset($_POST['experiencia-cargo']) ? 
+                      array(
+                        'cargos' => $_POST['experiencia-cargo'],
+                        'empresas' => $_POST['experiencia-empresa'],
+                        'inicios' =>  $_POST['experiencia-inicio'],
+                        'fins' =>  $_POST['experiencia-fim'],
+                      ) : null
+);
+
+require_once("modelos/{$modelo}.php");
---
+++ js/index.js
@@ -0,0 +1,68 @@
+$(function(){
+    
+    $("#btn-adicionar-formacao").click(function(e){
+        
+        e.preventDefault();
+
+        
+        var linha = '<div class="card">'+
+                    '    <div class="card-body">'+
+                    '        <div class="row">'+
+                    '            <div class="col-12 col-md-5">'+
+                    '                <input type="text" class="form-control" placeholder="Curso" name="formacao-curso[]">'+
+                    '            </div>'+
+                    '            <div class="col-12 col-md-5">'+
+                    '                <input type="text" class="form-control" placeholder="Instituição" name="formacao-instituicao[]">'+
+                    '            </div>'+
+                    '            <div class="col-12 col-md">'+
+                    '                <input type="text" class="form-control" placeholder="Conclusão" name="formacao-conclusao[]">'+
+                    '            </div>'+
+                    '            <div class="col-12 col-md">'+
+                    '                <button type="button" class="btn btn-danger form-control btn-remover-item" title="Remover formação">&times;</button>'+
+                    '            </div>'+
+                    '        </div>'+
+                    '    </div>'+
+                    '</div>';
+        
+        
+        $("#div-formacoes").append(linha);
+    });
+
+    
+    $("#btn-adicionar-experiencia").click(function(e){
+        
+        e.preventDefault();
+
+        
+        var linha = '<div class="card">'+
+                    '    <div class="card-body">'+
+                    '        <div class="row">'+
+                    '            <div class="col-12 col-md-4">'+
+                    '                <input type="text" class="form-control" placeholder="Cargo" name="experiencia-cargo[]">'+
+                    '            </div>'+
+                    '            <div class="col-12 col-md-3">'+
+                    '                <input type="text" class="form-control" placeholder="Empresa" name="experiencia-empresa[]">'+
+                    '            </div>'+
+                    '            <div class="col-6 col-md-2">'+
+                    '                <input type="text" class="form-control" placeholder="Início" name="experiencia-inicio[]">'+
+                    '            </div>'+
+                    '            <div class="col-6 col-md-2">'+
+                    '                <input type="text" class="form-control" placeholder="Fim" name="experiencia-fim[]">'+
+                    '            </div>'+
+                    '            <div class="col col-md">'+
+                    '                <button type="button" class="btn btn-danger btn-remover-item form-control" title="Remover experiência">&times;</button>'+
+                    '            </div>'+
+                    '        </div>'+
+                    '    </div>'+
+                    '</div>';
+        
+        
+        $("#div-experiencias").append(linha);
+    });
+
+    
+    $("#div-formacoes, #div-experiencias").on("click", ".btn-remover-item", function(){
+        
+        $(this).parent().parent().parent().parent().remove();
+    })
+});
